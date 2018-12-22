fa13c4c50d45d3926090f799135a82de88baf97e
perfectcjh <perfectcjh@foxmail.com>
2018-12-22 16:22:13 +0800

    bugfix

--- SwiftAppSample/Vendor/TPGCDTimer/TPGCDTimer.swift
+++ SwiftAppSample/Vendor/TPGCDTimer/TPGCDTimer.swift
@@ -8,51 +8,49 @@
 
 import UIKit
 
-typealias ActionBlock = () -> ()
-
 class TPGCDTimer: NSObject {
     
+    typealias ActionBlock = () -> Void
     static let shared = TPGCDTimer.init()
-
     lazy var timerContainer = [String: DispatchSourceTimer]()
     
     // GCD定时器
-    func scheduledDispatchTimer(WithTimerName name: String?, timeInterval: Double, queue: DispatchQueue, repeats: Bool, action: @escaping ActionBlock) {
+    func scheduledDispatchTimer(name: String,
+                                timeInterval: Double,
+                                queue: DispatchQueue? = .global(),
+                                repeats: Bool? = false,
+                                action: ActionBlock? = nil) {
         
-        if name == nil {
-            return
-        }
-        
-        var timer = timerContainer[name!]
+        var timer = timerContainer[name]
         if timer == nil {
             timer = DispatchSource.makeTimerSource(flags: [], queue: queue)
             timer?.resume()
-            timerContainer[name!] = timer
+            timerContainer[name] = timer
         }
         
         timer?.schedule(deadline: .now(), repeating: timeInterval, leeway: DispatchTimeInterval.milliseconds(100))
         timer?.setEventHandler(handler: { [weak self] in
-            action()
+            action?()
             if repeats == false {
-                self?.cancleTimer(name)
+                self?.cancleTimer(name: name)
             }
         })
     }
     
     // 取消定时器
-    func cancleTimer(_ name: String?) {
-        let timer = timerContainer[name!]
+    func cancleTimer(name: String) {
+        let timer = timerContainer[name]
         if timer == nil {
             return
         }
-        timerContainer.removeValue(forKey: name!)
+        timerContainer.removeValue(forKey: name)
         timer?.cancel()
     }
     
     
     // 检查定时器是否已存在
-    func isExistTimer(_ name: String?) -> Bool {
-        if timerContainer[name!] != nil {
+    func isExistTimer(name: String) -> Bool {
+        if timerContainer[name] != nil {
             return true
         }
         return false
